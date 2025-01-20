cb16f1231ed076c3a325381ee0fed6a329b8df93
chrisCodes141 <48960350+chrisCodes141@users.noreply.github.com>
2025-01-20 13:56:31 -0500

    Time: 6 ms (70.02%), Space: 44.8 MB (40.98%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,22 +1,39 @@
 class Solution {
-    public int lengthOfLongestSubstring(String s) {       int stringLength = s.length();
-        int lengthOfLongestSubstring  = 0;
+    public int lengthOfLongestSubstring(String s) {
+    //     int stringLength = s.length();
+    //     int lengthOfLongestSubstring  = 0;
 
-        //i is left pointer, j is righter pointer
-        for (int i = 0; i < stringLength; i++){
+    //     //i is left pointer, j is righter pointer
+    //     for (int i = 0; i < stringLength; i++){
 
-            HashSet set = new HashSet();
+    //         HashSet set = new HashSet();
 
-            //iterate starting from where left pointer is
-            for (int j = i; j < stringLength; j++){
+    //         //iterate starting from where left pointer is
+    //         for (int j = i; j < stringLength; j++){
 
-                //returns false if duplicate
-                if(!set.add(s.charAt(j))) break;
+    //             //returns false if duplicate
+    //             if(!set.add(s.charAt(j))) break;
 
-                //if window larger, update
-                lengthOfLongestSubstring = Math.max(lengthOfLongestSubstring, j - i + 1);
-            }
-        }
-        return lengthOfLongestSubstring;
+    //             //if window larger, update
+    //             lengthOfLongestSubstring = Math.max(lengthOfLongestSubstring, j - i + 1);
+    //         }
+    //     }
+    //     return lengthOfLongestSubstring;
+    // }
+
+    int n = s.length(),left=0,right=0,maxLen=0;
+      Set<Character> set = new HashSet<>();
+      while(right<n)
+      {
+         while(set.contains(s.charAt(right)))
+         {
+            set.remove(s.charAt(left));
+            left++;
+         }
+         set.add(s.charAt(right));
+         maxLen = Math.max(maxLen,right-left+1);
+         right++;
+      }
+     return maxLen;
     }
 }
